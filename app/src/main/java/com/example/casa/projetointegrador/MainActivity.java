b7a5f7adcfa3552bebe0b92faf46ea8189626717
Myller Claudino de Freitas <myllercf@gmail.com>
2017-04-01 03:33:12 -0300

    Initial commit

---
+++ app/src/main/java/com/example/casa/projetointegrador/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.casa.projetointegrador;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    Float alt;
+    Float massa;
+    Float imc;
+    boolean masculino;
+    boolean feminino;
+
+    EditText altura;
+    EditText peso;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        altura = (EditText) findViewById(R.id.altura);
+        peso = (EditText) findViewById(R.id.peso);
+
+        Button botao = (Button) findViewById(R.id.button);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                alt = Float.parseFloat(altura.getText().toString());
+                massa = Float.parseFloat(peso.getText().toString());
+
+                imc = massa / alt*alt;
+
+                RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+                masculino = R.id.radioMasculino == radioGroup.getCheckedRadioButtonId();
+                feminino = R.id.radioFeminino == radioGroup.getCheckedRadioButtonId();
+
+                Intent tela2 = new Intent(MainActivity.this, Resultado.class);
+
+                Bundle resultado = new Bundle();
+                resultado.putFloat("imc", imc);
+
+                if (masculino){
+                    resultado.putString("sexo", "masculino");
+                } else{
+                    resultado.putString("sexo", "feminino");
+                }
+
+                startActivity(tela2);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/casa/projetointegrador/Resultado.java
@@ -0,0 +1,11 @@
+package com.example.casa.projetointegrador;
+
+import android.app.Activity;
+
+/**
+ * Created by casa on 01/04/2017.
+ */
+
+public class Resultado extends Activity {
+
+}
